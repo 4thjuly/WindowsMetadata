70c69e0eafa27cda3cfeff92697dbfaf60ea6127
4thjuly <ian@ellison-taylor.com>
2021-02-07 23:31:39 -0800

    Load reader

--- test.jl
+++ test.jl
@@ -21,7 +21,33 @@
 #         IUnknown    **ppIUnk) PURE;         // [out] Return interface on success.
 # };
 
-const Byte = UInt8
+# //Open the winmd file we want to dump
+# String filename = "C:\Windows\System32\WinMetadata\Windows.Globalization.winmd";
+
+# IMetaDataImport reader; //IMetadataImport2 supports generics
+# dispenser.OpenScope(filename, ofRead, IMetaDataImport, out reader); //"Import" is used to read metadata. "Emit" is used to write metadata.
+
+# const Byte = UInt8
+# Pointer enum = null;
+# mdTypeDef typeID;
+# Int32 nRead;
+# while (reader.EnumTypeDefs(enum, out typeID, 1, out nRead) = S_OK)
+# {
+#    ProcessToken(reader, typeID);
+# }
+# reader.CloseEnum(enum);
+
+# void ProcessToken(IMetaDataImport reader, mdTypeDef typeID)
+# {
+#    //Get three interesting properties of the token:
+#    String      typeName;       //e.g. "Windows.Globalization.NumberFormatting.DecimalFormatter"
+#    UInt32      ancestorTypeID; //the token of this type's ancestor (e.g. Object, Interface, System.ValueType, System.Enum)
+#    CorTypeAttr flags;          //various flags about the type (e.g. public, private, is an interface)
+
+#    GetTypeInfo(reader, typeID, out typeName, out ancestorTypeID, out flags);
+# }
+
+
 
 struct GUID
     Data1::Culong
@@ -52,6 +78,7 @@ end
 
 const CLSID_CorMetaDataDispenser = guid"E5CB7A31-7512-11d2-89CE-0080C792E5D8"
 const IID_IMetaDataDispenser = guid"809C652E-7396-11D2-9771-00A0C9B4D50C"
+const IID_IMetaDataImport = guid"7DAC8207-D3AE-4C75-9B67-92801A497D44"
 
 # TODO - Make Interface definitions a macro?
 struct IMetaDataDispenserVtbl
@@ -70,9 +97,93 @@ rpmdd = Ref(Ptr{IMetaDataDispenser}(C_NULL))
 res = @ccall "Rometadata".MetaDataGetDispenser(
     Ref(CLSID_CorMetaDataDispenser)::Ptr{Cvoid}, 
     Ref(IID_IMetaDataDispenser)::Ptr{Cvoid}, 
-    rpmdd::Ptr{Ptr{IMetaDataDispenser}})::Cuint
+    rpmdd::Ptr{Ptr{IMetaDataDispenser}})::HRESULT
 
 # Test
 mdd = unsafe_load(rpmdd[])
 vtbl = unsafe_load(mdd.pvtbl)
 dump(vtbl)
+
+
+
+
+
+
+struct IMetaDataImportVtbl
+    CloseEnum::Ptr{Cvoid}
+    CountEnum::Ptr{Cvoid}
+    EnumCustomAttributes::Ptr{Cvoid}
+    EnumEvents::Ptr{Cvoid}
+    EnumFields::Ptr{Cvoid}
+    EnumFieldsWithName::Ptr{Cvoid}
+    EnumInterfaceImpls::Ptr{Cvoid}
+    EnumMemberRefs::Ptr{Cvoid}
+    EnumMembers::Ptr{Cvoid}
+    EnumMembersWithName::Ptr{Cvoid}
+    EnumMethodImpls::Ptr{Cvoid}
+    EnumMethods::Ptr{Cvoid}
+    EnumMethodSemantics::Ptr{Cvoid}
+    EnumMethodsWithName::Ptr{Cvoid}
+    EnumModuleRefs::Ptr{Cvoid}
+    EnumParams::Ptr{Cvoid}
+    EnumPermissionSets::Ptr{Cvoid}
+    EnumProperties::Ptr{Cvoid}
+    EnumSignatures::Ptr{Cvoid}
+    EnumTypeDefs::Ptr{Cvoid}
+    EnumTypeRefs::Ptr{Cvoid}
+    EnumTypeSpecs::Ptr{Cvoid}
+    EnumUnresolvedMethods::Ptr{Cvoid}
+    EnumUserStrings::Ptr{Cvoid}
+    FindField::Ptr{Cvoid}
+    FindMember::Ptr{Cvoid}
+    FindMemberRef::Ptr{Cvoid}
+    FindMethod::Ptr{Cvoid}
+    FindTypeDefByName::Ptr{Cvoid}
+    FindTypeRef::Ptr{Cvoid}
+    GetClassLayout::Ptr{Cvoid}
+    GetCustomAttributeByName::Ptr{Cvoid}
+    GetCustomAttributeProps::Ptr{Cvoid}
+    GetEventProps::Ptr{Cvoid}
+    GetFieldMarshal::Ptr{Cvoid}
+    GetFieldProps::Ptr{Cvoid}
+    GetInterfaceImplProps::Ptr{Cvoid}
+    GetMemberProps::Ptr{Cvoid}
+    GetMemberRefProps::Ptr{Cvoid}
+    GetMethodProps::Ptr{Cvoid}
+    GetMethodSemantics::Ptr{Cvoid}
+    GetModuleFromScope::Ptr{Cvoid}
+    GetModuleRefProps::Ptr{Cvoid}
+    GetNameFromToken::Ptr{Cvoid}
+    GetNativeCallConvFromSig::Ptr{Cvoid}
+    GetNestedClassProps::Ptr{Cvoid}
+    GetParamForMethodIndex::Ptr{Cvoid}
+    GetParamProps::Ptr{Cvoid}
+    GetPermissionSetProps::Ptr{Cvoid}
+    GetPinvokeMap::Ptr{Cvoid}
+    GetPropertyProps::Ptr{Cvoid}
+    GetRVA::Ptr{Cvoid}
+    GetScopeProps::Ptr{Cvoid}
+    GetSigFromToken::Ptr{Cvoid}
+    GetTypeDefProps::Ptr{Cvoid}
+    GetTypeRefProps::Ptr{Cvoid}
+    GetTypeSpecFromToken::Ptr{Cvoid}
+    GetUserString::Ptr{Cvoid}
+    IsGlobal::Ptr{Cvoid}
+    IsValidToken::Ptr{Cvoid}
+    ResetEnum::Ptr{Cvoid}
+    ResolveTypeRef::Ptr{Cvoid}
+end
+struct IMetaDataImport
+    pvtbl::Ptr{IMetaDataImportVtbl}
+end
+
+const CorOpenFlags_ofRead = 0x00000000;
+
+# Can't use @ccall
+rmdi = Ref(Ptr{IMetaDataImport}(C_NULL)) 
+res = ccall(vtbl.OpenScope, HRESULT, (Ptr{IMetaDataDispenser}, Cwstring, Cuint, Ptr{Cvoid}, Ptr{Ptr{IMetaDataImport}}), 
+    rpmdd[], "Windows.Win32.winmd", CorOpenFlags_ofRead, Ref(IID_IMetaDataImport), rmdi)
+mdi = unsafe_load(rmdi[])
+mtdi_vtbl = unsafe_load(mdi.pvtbl)
+dump(mtdi_vtbl)
+
