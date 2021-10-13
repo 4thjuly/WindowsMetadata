83470c9d2022cd888c1b5bd52e3f6fbfe782a0ad
july4th <4thjuly@users.noreply.github.com>
2021-10-12 18:25:14 -0700

    ...

--- testMetadataimport.jl
+++ testMetadataimport.jl
@@ -1,5 +1,5 @@
 include("metadataimport-wrapper.jl")
-include("winmd.jl")
+# include("winmd.jl")
 
 mdd = metadataDispenser()
 @show mdd
@@ -12,124 +12,124 @@ mdRegClass = findMethod(mdi, tdWAMApis, "RegisterClassExW")
 @show mdRegClass
 println()
 
-(moduleref, importname) = getPInvokeMap(mdi, mdRegClass)
-@show importname
-println()
-
-moduleName = getModuleRefProps(mdi, moduleref)
-@show moduleName
-println()
-
-paramDef = getParamForMethodIndex(mdi, mdRegClass, 1) 
-paramName = getParamProps(mdi, paramDef)
-@show paramName
-println()
-
-sigblob = getMethodProps(mdi, mdRegClass)
-@show sigblob
-println()
-
-types = methodSigblobToTypeInfos(sigblob)
-@show types[2]
-typedref = types[2][1]
-
-@show typedref
-@show isValidToken(mdi, typedref)
-
-structname = getTypeRefName(mdi, typedref)
-@show structname
-println()
-
-# Dump struct
-function showFields(fields::Vector{mdFieldDef})
-    for field in fields
-        name, sigblob = getFieldProps(mdi, field)
-        @show name
-        @show sigblob
-        @show fieldSigblobToTypeInfo(sigblob)
-    end
-end
-
-structToken = findTypeDef(mdi, structname)
-@show structToken
-tdprops = getTypeDefProps(mdi, structToken)
-@show tdprops.extends
-@show getTypeRefName(mdi, tdprops.extends)
-println()
-
-fields = enumFields(mdi, structToken)
-showFields(fields)
-println()
-
-# Drill in to last field
-_, sigblob = getFieldProps(mdi, fields[end])
-name = getName(mdi, fieldSigblobToTypeInfo(sigblob)[1])
-@show name
-enumFields(mdi, findTypeDef(mdi, name)) |> showFields
-println()
-
-# convert 
-undotname = convertTypeNameToJulia(name)
-name, sigblob = getFieldProps(mdi, enumFields(mdi, findTypeDef(mdi, name))[1])
-@show name
-typeinfo = fieldSigblobToTypeInfo(sigblob)
-@show typeinfo[1]
-# jt = convertTypeToJulia(mdi, typeinfo[1])
-# createStructType(undotname, [(fps.name, jt)])
-
-# Test
-# hicon = Windows_Win32_Gdi_HICON(42)
-# dump(hicon)
+# (moduleref, importname) = getPInvokeMap(mdi, mdRegClass)
+# @show importname
 # println()
 
-# WndProc
-_, sigblob = getFieldProps(mdi, fields[3])
-field3type,_ = fieldSigblobToTypeInfo(sigblob)
-@show field3type
-name = getName(mdi, field3type)
-@show name
-tdWndProc = findTypeDef(mdi, name)
-name, ext = getTypeDefProps(mdi, tdWndProc)
-extendsName = getName(mdi, ext)
-@show extendsName
-wndprocMemnbers = enumMembers(mdi, tdWndProc)
-@show wndprocMemnbers
-println()
+# moduleName = getModuleRefProps(mdi, moduleref)
+# @show moduleName
+# println()
 
-# PAINTSTRUCT
-structToken = findTypeDef(mdi, "Windows.Win32.Gdi.PAINTSTRUCT")
-fields = enumFields(mdi, structToken)
-showFields(fields)
-println()
+# paramDef = getParamForMethodIndex(mdi, mdRegClass, 1) 
+# paramName = getParamProps(mdi, paramDef)
+# @show paramName
+# println()
 
-# Enums
-structToken = findTypeDef(mdi, "Windows.Win32.SystemServices.Apis")
-fields = enumFields(mdi, structToken)
-@show length(fields)
-println()
+# sigblob = getMethodProps(mdi, mdRegClass)
+# @show sigblob
+# println()
 
-# More method stuff
-function showParams(mdi::CWMetaDataImport, params::Vector{mdParamDef}, paramtypes::Vector{Tuple{mdTypeDef, UInt32, Int}})
-    for i = 1:length(params)
-        name = getParamProps(mdi, params[i])
-        type = paramtypes[i][1]
-        @show name type
-    end
-end
+# types = methodSigblobToTypeInfos(sigblob)
+# @show types[2]
+# typedref = types[2][1]
 
-method = "GetModuleHandleExW"
-@show method
-tdapis = findTypeDef(mdi, "Windows.Win32.SystemServices.Apis")
-@show tdapis
-mdgmh = findMethod(mdi, tdapis, method)
-@show mdgmh
-mref, importname = getPInvokeMap(mdi, mdgmh)
-moduleName = getModuleRefProps(mdi, mref)
-@show moduleName importname
-sigblob = getMethodProps(mdi, mdgmh)
-@show sigblob
-paramtypes = methodSigblobToTypeInfos(sigblob)
-@show paramtypes
-params = enumParams(mdi, mdgmh)
-showParams(mdi, params, paramtypes)
-println()
+# @show typedref
+# @show isValidToken(mdi, typedref)
+
+# structname = getTypeRefName(mdi, typedref)
+# @show structname
+# println()
+
+# # Dump struct
+# function showFields(fields::Vector{mdFieldDef})
+#     for field in fields
+#         name, sigblob = getFieldProps(mdi, field)
+#         @show name
+#         @show sigblob
+#         @show fieldSigblobToTypeInfo(sigblob)
+#     end
+# end
+
+# structToken = findTypeDef(mdi, structname)
+# @show structToken
+# tdprops = getTypeDefProps(mdi, structToken)
+# @show tdprops.extends
+# @show getTypeRefName(mdi, tdprops.extends)
+# println()
+
+# fields = enumFields(mdi, structToken)
+# showFields(fields)
+# println()
+
+# # Drill in to last field
+# _, sigblob = getFieldProps(mdi, fields[end])
+# name = getName(mdi, fieldSigblobToTypeInfo(sigblob)[1])
+# @show name
+# enumFields(mdi, findTypeDef(mdi, name)) |> showFields
+# println()
+
+# # convert 
+# undotname = convertTypeNameToJulia(name)
+# name, sigblob = getFieldProps(mdi, enumFields(mdi, findTypeDef(mdi, name))[1])
+# @show name
+# typeinfo = fieldSigblobToTypeInfo(sigblob)
+# @show typeinfo[1]
+# # jt = convertTypeToJulia(mdi, typeinfo[1])
+# # createStructType(undotname, [(fps.name, jt)])
+
+# # Test
+# # hicon = Windows_Win32_Gdi_HICON(42)
+# # dump(hicon)
+# # println()
+
+# # WndProc
+# _, sigblob = getFieldProps(mdi, fields[3])
+# field3type,_ = fieldSigblobToTypeInfo(sigblob)
+# @show field3type
+# name = getName(mdi, field3type)
+# @show name
+# tdWndProc = findTypeDef(mdi, name)
+# name, ext = getTypeDefProps(mdi, tdWndProc)
+# extendsName = getName(mdi, ext)
+# @show extendsName
+# wndprocMemnbers = enumMembers(mdi, tdWndProc)
+# @show wndprocMemnbers
+# println()
+
+# # PAINTSTRUCT
+# structToken = findTypeDef(mdi, "Windows.Win32.Gdi.PAINTSTRUCT")
+# fields = enumFields(mdi, structToken)
+# showFields(fields)
+# println()
+
+# # Enums
+# structToken = findTypeDef(mdi, "Windows.Win32.SystemServices.Apis")
+# fields = enumFields(mdi, structToken)
+# @show length(fields)
+# println()
+
+# # More method stuff
+# function showParams(mdi::CWMetaDataImport, params::Vector{mdParamDef}, paramtypes::Vector{Tuple{mdTypeDef, UInt32, Int}})
+#     for i = 1:length(params)
+#         name = getParamProps(mdi, params[i])
+#         type = paramtypes[i][1]
+#         @show name type
+#     end
+# end
+
+# method = "GetModuleHandleExW"
+# @show method
+# tdapis = findTypeDef(mdi, "Windows.Win32.SystemServices.Apis")
+# @show tdapis
+# mdgmh = findMethod(mdi, tdapis, method)
+# @show mdgmh
+# mref, importname = getPInvokeMap(mdi, mdgmh)
+# moduleName = getModuleRefProps(mdi, mref)
+# @show moduleName importname
+# sigblob = getMethodProps(mdi, mdgmh)
+# @show sigblob
+# paramtypes = methodSigblobToTypeInfos(sigblob)
+# @show paramtypes
+# params = enumParams(mdi, mdgmh)
+# showParams(mdi, params, paramtypes)
+# println()
