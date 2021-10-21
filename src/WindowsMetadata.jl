2df2dd000a3713eca3c2e6ebe19a578df6bee02d
july4th <4thjuly@users.noreply.github.com>
2021-10-21 00:38:07 -0700

    ...

--- src/WindowsMetadata.jl
+++ src/WindowsMetadata.jl
@@ -122,7 +122,7 @@ function createStructType(structname::String, fields::Vector{Tuple{String, Type}
         end
     end
     eval(sexp)
-    # eval(Expr(:export, $(Symbol(structname))))
+    eval(Expr(:export, $(Symbol(structname))))
     return eval(Symbol(structname))
 end
 
